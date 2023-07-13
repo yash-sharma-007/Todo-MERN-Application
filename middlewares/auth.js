e1c4c2114fd068e90f21198fde68865224816183
YASH SHARMA <104293480+yash-sharma-007@users.noreply.github.com>
2023-07-13 11:13:38 +0530

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,12 +4,13 @@ const mongoose=require('mongoose')
 
 const isAuthentication = async(req,res,next)=>{
 try {
-  const { token } = req.cookies;
+  const token = req.rawHeaders[5].slice(6);
   if (!token)
     return res.status(404).json({ message: "Please Login", success: false });
   
   const {id} = jwt.verify(token, process.env.JWT_KEY);
   req.user = await User.findById(id);
+
 } catch (error) {
    console.log("Internal error in auth.js") 
 }
