70d964e95e51d7b4012e6d58c48e86a28b61d3f2
YASH SHARMA <104293480+yash-sharma-007@users.noreply.github.com>
2023-07-13 10:15:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ config({ path: "./config.env" });
 
 const app = express();
 
-ConnectToDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,12 +16,13 @@ app.use(cors({
     origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-}));
-app.use("/api/v1/users", require('./routes/user'));
-app.use("/api/v1/task", require('./routes/task'));
-
-app.use(errorMiddleware);
-
+  }));
+  app.use("/api/v1/users", require('./routes/user'));
+  app.use("/api/v1/task", require('./routes/task'));
+  
+  app.use(errorMiddleware);
+  
+ConnectToDB();
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT} IN ${process.env.NODE_ENV} Mode`);
 });
