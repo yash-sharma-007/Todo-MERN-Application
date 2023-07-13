05056c909d7be6fcdf9c1c344ce5448609b7d055
YASH SHARMA <104293480+yash-sharma-007@users.noreply.github.com>
2023-07-13 11:20:36 +0530

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,20 +1,27 @@
+
 const User = require('../models/user');
-const jwt=require('jsonwebtoken')
-const mongoose=require('mongoose')
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 
-const isAuthentication = async(req,res,next)=>{
-try {
-  const token = req.rawHeaders[5].slice(6);
-  if (!token)
-    return res.status(404).json({ message: "Please Login", success: false });
-  
-  const {id} = jwt.verify(token, process.env.JWT_KEY);
-  req.user = await User.findById(id);
+const isAuthentication = async (req, res, next) => {
+  try {
+    const cookieHeader = req.headers.cookie;
+    if (!cookieHeader) {
+      return res.status(404).json({ message: "Please Login", success: false });
+    }
 
-} catch (error) {
-   console.log("Internal error in auth.js") 
-}
-    next();
+    const tokenCookie = cookieHeader.split(';').find((cookie) => cookie.trim().startsWith('token='));
+    if (!tokenCookie) {
+      return res.status(404).json({ message: "Please Login", success: false });
+    }
 
-}
-module.exports=isAuthentication;
+    const token = tokenCookie.split('=')[1];
+    const { id } = jwt.verify(token, process.env.JWT_KEY);
+    req.user = await User.findById(id);
+  } catch (error) {
+    console.log("Internal error in auth.js", error);
+  }
+  next();
+};
+
+module.exports = isAuthentication;
