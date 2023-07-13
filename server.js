b0f8364092718e37a4512806d95e962930e58982
YASH SHARMA <104293480+yash-sharma-007@users.noreply.github.com>
2023-07-13 10:11:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const ConnectToDB = require("./db");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const {errorMiddleware}=require('./middlewares/error')
 const config = require("dotenv").config;
 config({ path: "./config.env" });
 
@@ -11,22 +12,16 @@ ConnectToDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 app.use(cors({
     origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
-app.use(cookieParser())
 app.use("/api/v1/users", require('./routes/user'));
 app.use("/api/v1/task", require('./routes/task'));
 
-app.use((err,req,res,next)=>{
-  err.message=err.message || "Internal Error";
-  res.status(500).json({
-    success:false,
-    message:err.message,
-}); 
-})
+app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT} IN ${process.env.NODE_ENV} Mode`);
