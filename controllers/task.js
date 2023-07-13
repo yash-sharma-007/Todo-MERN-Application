e62eb8b8baf2782789780f9165a06fa88c5e1de8
YASH SHARMA <104293480+yash-sharma-007@users.noreply.github.com>
2023-07-13 22:12:30 +0530

    Update task.js

--- controllers/task.js
+++ controllers/task.js
@@ -5,13 +5,7 @@ const User=require('../models/user')
 const newTask = async(req,res,next) =>{
     try{  
         const {title,description}=req.body;
-        const token=req.header('auth-token')
-        if (!token)
-       return res.status(404).json({ message: "Please Login", success: false });
-     
-      const {id} = jwt.verify(token, process.env.JWT_KEY);
-        const user = await User.findById(id);
-          await Task.create({title,description,user});
+          await Task.create({title,description,user:req.user});
         res.status(201).json({
             success:true,
             message:'Task added successfully',  
@@ -23,13 +17,7 @@ const newTask = async(req,res,next) =>{
 
 const getMyTask = async(req,res,next)=>{
     try{
-        const {token}=req.body;
-        if (!token)
-            return res.status(404).json({ message: "Please Login", success: false });
-        const {id} = jwt.verify(token, process.env.JWT_KEY);
-        const user = await User.findById(id);
-        const userId=user._id;
-        const tasks=await Task.find({user:userId});
+        const tasks=await Task.find({user:req.user}); 
         res.status(201).json({success:true,
             tasks,
         });
@@ -41,16 +29,10 @@ const getMyTask = async(req,res,next)=>{
 const UpdateTask = async(req,res,next)=>{
     
     try {   
-        const {token}=req.body;
-        if (!token)
-            return res.status(404).json({ message: "Please Login", success: false });
-        const {id} = jwt.verify(token, process.env.JWT_KEY);
-        const user = await User.findById(id);
-        const userId=user._id;
+       
         const task=await Task.findById(req.params.taskid);
-        console.log(task)
         if(!task) return next(new Error("Task Not Found"))
-        if(JSON.stringify(task.user._id)!=JSON.stringify(userId)) return next(new Error("Task Not Found"))
+        if(JSON.stringify(task.user)!=JSON.stringify(req.user)) return next(new Error("Task Not Found"))
         task.isCompleted=!task.isCompleted;
         await task.save();
         res.status(201).json({
@@ -66,16 +48,10 @@ const UpdateTask = async(req,res,next)=>{
 const DeleteTask = async(req,res,next)=>{
 
     try{
-        const {token}=req.body;
-        if (!token)
-            return res.status(404).json({ message: "Please Login", success: false });
-        const {id} = jwt.verify(token, process.env.JWT_KEY);
-        const user = await User.findById(id);
-        const userId=user._id;
+        
         const task=await Task.findById(req.params.taskid);
-        console.log(task)
         if(!task) return next(new Error("Task Not Found"))
-        if(JSON.stringify(task.user._id)!=JSON.stringify(userId)) return next(new Error("Task Not Found"))
+        if(JSON.stringify(task.user)!=JSON.stringify(req.user)) return next(new Error("Task Not Found"))
         await task.deleteOne();
         res.status(201).json({
             success:true,
