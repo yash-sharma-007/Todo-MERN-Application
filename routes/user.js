c39b75a826b870e90edf3fd466243006c1259653
YASH SHARMA <104293480+yash-sharma-007@users.noreply.github.com>
2023-07-13 22:22:27 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,12 @@
 const express=require('express')
 const router=express.Router();
 const{ Register,Login,Mydetails,Logout}=require('../controllers/user')
-const isAuthentication=require('../middlewares/auth')
+const fetchuser=require('../middlewares/fetchuser')
 
 
 router.post('/register',Register);
 router.post('/login',Login);
-router.get('/me',Mydetails);
+router.get('/me',fetchuser,Mydetails);
 router.get('/logout',Logout);
 
 module.exports=router;
