bd55d6700a759b239fae5b242e93e6dcb46bcfb7
YASH SHARMA <104293480+yash-sharma-007@users.noreply.github.com>
2023-07-13 22:14:12 +0530

    Update feature.js

--- functions/feature.js
+++ functions/feature.js
@@ -1,16 +1,16 @@
 const jwt = require('jsonwebtoken');
 
 const sendCookie = (user, res, message, statuscode=200) => {
-  const token = jwt.sign({ _id: user._id }, process.env.JWT_KEY);
+  const token = jwt.sign({ id: user._id }, process.env.JWT_KEY);
   res
     .status(statuscode)
     .cookie("token", token, {
       httpOnly: true,
-      maxAge: 15 * 60 * 1000,
+      maxage: 15 * 60 * 1000,
       sameSite: process.env.NODE_ENV === "DEVELOPMENT" ? "lax" : "none",
       secure: process.env.NODE_ENV === "DEVELOPMENT" ? false : true,
-    })
-    .json({ success: true, message: message,cookie:token,user:user });
+    }) 
+    .json({ success: true, message: message,token:token,user:user });
 };
 
 module.exports = sendCookie;
