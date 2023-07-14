b4ac30d6ea1f5adff2129717f1b06fb2dce025e5
YASH SHARMA <104293480+yash-sharma-007@users.noreply.github.com>
2023-07-14 11:49:36 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,21 @@ const express=require('express')
 const router=express.Router();
 const{ Register,Login,Mydetails,Logout}=require('../controllers/user')
 const fetchuser=require('../middlewares/fetchuser')
+const { body, validationResult } = require('express-validator');
+
+router.post('/register',[
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
+  ], Register);
 
 
-router.post('/register',Register);
-router.post('/login',Login);
+router.post('/login',[
+  body('email', 'Enter a valid email').isEmail(),
+  body('password', 'Password cannot be blank').exists(),
+],Login);
+
+
 router.get('/me',fetchuser,Mydetails);
 router.get('/logout',Logout);
 
