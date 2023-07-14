7ebbe07288ce6b208bc2f3e3002c2c0cdda9aa88
YASH SHARMA <104293480+yash-sharma-007@users.noreply.github.com>
2023-07-14 11:50:10 +0530

    Update task.js

--- routes/task.js
+++ routes/task.js
@@ -1,10 +1,30 @@
-const express=require('express')
-const {newTask,getMyTask,UpdateTask, DeleteTask}=require('../controllers/task');
-const router=express.Router();
-const fetchuser=require('../middlewares/fetchuser')
+const express = require("express");
+const {
+  newTask,
+  getMyTask,
+  UpdateTask,
+  DeleteTask,
+} = require("../controllers/task");
+const router = express.Router();
+const fetchuser = require("../middlewares/fetchuser");
+const { body, validationResult } = require("express-validator");
 
-router.post('/new',fetchuser, newTask);
-router.get('/fetchtask',fetchuser,getMyTask);
-router.route('/:taskid').put(fetchuser,UpdateTask).delete(fetchuser,DeleteTask);
+router.post(
+  "/new",
+  fetchuser,
+  [
+    body("title", "Enter a valid title").isLength({ min: 3 }),
+    body("description", "Description must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  newTask
+);
 
-module.exports=router;
+router.get("/fetchtask", fetchuser, getMyTask);
+router
+  .route("/:taskid")
+  .put(fetchuser, UpdateTask)
+  .delete(fetchuser, DeleteTask);
+
+module.exports = router;
